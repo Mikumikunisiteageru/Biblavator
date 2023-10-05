7e812f32b40b6884c8a66ea6d91376d61121fccc
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-10-05 13:28:35 +0800

    refactor: clean code

--- biblavator/__init__.py
+++ biblavator/__init__.py
@@ -7,6 +7,40 @@ import os
 import pikepdf
 import zipfile
 
+class Path:
+	def __init__(self, dir, core, ext):
+		self.dir = dir
+		self.core = core
+		self.ext = ext
+	def __eq__(self, another):
+		return self.dir == another.dir and \
+			self.core == another.core and self.ext == another.ext
+	def copy(self):
+		return Path(self.dir, self.core, self.ext)
+	def isa(self, ext):
+		return self.ext == ext
+	def join(self):
+		return os.path.join(self.dir, self.core + self.ext)
+	def inheritdir(self, another):
+		if self.dir == "":
+			self.dir = another.dir
+		return self
+	def suffixcore(self, suffix):
+		self.core += suffix
+		return self
+	def supplyext(self, ext):
+		if self.ext == "":
+			self.ext = ext
+		return self
+	def forceext(self, ext):
+		self.ext = ext
+		return self
+
+def splitpath(string):
+	dir, filename = os.path.split(string)
+	core, ext = os.path.splitext(filename)
+	return Path(dir, core, ext)
+
 def dewatermark(pdf):
 	for page in pdf.pages:
 		try:
@@ -14,27 +48,30 @@ def dewatermark(pdf):
 		except:
 			pass
 
-def pdflava(spath, tpath, password=""):
-	with pikepdf.open(spath, password=password, 
+def pdflava(srcpath, dscpath, password=""):
+	with pikepdf.open(srcpath.join(), password=password, 
 			allow_overwriting_input=True) as pdf:
 		dewatermark(pdf)
-		pdf.save(tpath)
+		pdf.save(dscpath.join())
 
-def ziplava(spath, tpath):
+def ziplava(srcpath, dscpath):
 	with pikepdf.new() as pdf:
-		fzip = zipfile.ZipFile(spath)
-		for fname in sorted(fzip.namelist()):
-			htmlbytes = fzip.read(fname)
-			x = htmlbytes.find(b"var content_data =")
-			i = htmlbytes.find(b"\"", x)
-			j = htmlbytes.find(b"\"", i + 1)
-			pdfbytes = base64.b64decode(htmlbytes[i+1:j])
-			with pikepdf.open(io.BytesIO(pdfbytes)) as subpdf:
-				pdf.pages.extend(subpdf.pages)
+		with zipfile.ZipFile(srcpath.join()) as fzip:
+			for fname in sorted(fzip.namelist()):
+				htmlbytes = fzip.read(fname)
+				posref = htmlbytes.find(b"var content_data =")
+				poslqt = htmlbytes.find(b"\"", posref)
+				posrqt = htmlbytes.find(b"\"", poslqt + 1)
+				pdfbytes = base64.b64decode(htmlbytes[poslqt+1:posrqt])
+				with pikepdf.open(io.BytesIO(pdfbytes)) as subpdf:
+					pdf.pages.extend(subpdf.pages)
 		dewatermark(pdf)
-		pdf.save(tpath)
+		pdf.save(dscpath.join())
 
-def main():
+def removefile(path):
+	os.remove(path.join())
+
+def parseargs():
 	parser = argparse.ArgumentParser(
 		description = "Clean PDF files off watermarks or access restriction.")
 	parser.add_argument("filename", nargs="+", metavar="FILENAME", type=str, 
@@ -43,27 +80,25 @@ def main():
 		help = "password protecting the PDF file")
 	parser.add_argument("-k", "--keep", action="store_true", 
 		help = "keep the original file after cleaning up")
-	args = parser.parse_args()
-	spath = args.filename[0]
-	sdir, sname = os.path.split(spath)
-	score, sext = os.path.splitext(sname)
-	if sext == "":
-		sext = ".pdf"
-		spath += ".pdf"
+	return parser.parse_args()
+
+def extractpaths(args):
+	srcpath = splitpath(args.filename[0]).supplyext(".pdf")
 	if len(args.filename) == 1:
-		tpath = os.path.join(sdir, score + "_lavatus" + ".pdf")
+		dstpath = srcpath.copy().suffixcore("_lavatus")
 	else:
-		tdir, args.filename[1] = os.path.split(args.filename[1])
-		if tdir == "":
-			tdir = sdir
-		if not args.filename[-1].endswith(".pdf"):
-			args.filename[-1] += ".pdf"
-		tpath = os.path.join(tdir, ' '.join(args.filename[1:]))
-	if sext == ".pdf":
-		pdflava(spath, tpath, password=args.password)
-	elif sext == ".zip":
-		ziplava(spath, tpath)
+		dstpath = splitpath(' '.join(args.filename[1:])).inheritdir(srcpath)
+	dstpath.forceext(".pdf")
+	return srcpath, dstpath
+
+def main():
+	args = parseargs()
+	srcpath, dstpath = extractpaths(args)
+	if srcpath.isa(".pdf"):
+		pdflava(srcpath, dstpath, password=args.password)
+	elif srcpath.isa(".zip"):
+		ziplava(srcpath, dstpath)
 	else:
 		raise ValueError("File format unrecognized!")
-	if not args.keep:
-		os.remove(spath)
+	if srcpath != dstpath and not args.keep:
+		removefile(srcpath)
